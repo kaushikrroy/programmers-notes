130df74046f00686086326741dafcc8b95aeb938
Kaushik Roy <kaushikrroy@gmail.com>
2018-02-01 09:55:07 +0530

    Adding few more problems.

---
+++ src/main/java/io/github/kaushikrroy/programmers/java/notes/random/ShortestDeliveryPath.java
@@ -0,0 +1,28 @@
+package io.github.kaushikrroy.programmers.java.notes.random;
+
+/**
+ * <p>A delivery boy wants to deliver some items on his way from office to home. You need to find the optimized path he
+ * should take from office to home and deliver all his deliveries on his way.
+ * <br>
+ * <br>It is 101 X 101 grid. Office, home , delivery points are represented via coordinated (x,y) where 0 &lt;= x &lt;= 100,
+ * 0 &lt;= y &lt;= 100.
+ * <br>
+ * <br>distance between two points (x1, y1) and (x2,y2) is computed as |x1 - x2| + |y1 - y2|
+ * <br>
+ * <br>You need to find the optimized path from office to home covering all delivery locations and return the
+ * optimized path length as output.
+ * <br>
+ * <br>You will be given the input in the 2 lines
+ * <br>
+ * <br>first line - N (no. of delivery locations)
+ * <br>second line - (x,y) coordinates of office, followed by home, followed by all N delivery locations.
+ * <br>
+ * <br>3
+ * <br>0 0 100 100 20 30 50 50 70 70
+ * <br>
+ * <br>output: The length of the optimized path taken.
+ * <br>
+ * <br>For above input, the output is 200</p>
+ */
+public class ShortestDeliveryPath {
+}
