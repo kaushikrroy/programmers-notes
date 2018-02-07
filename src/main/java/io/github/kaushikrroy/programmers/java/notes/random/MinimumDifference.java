99767ac1481d65a2962933d5aaab53957fa6b3a7
Kaushik Roy <kaushikrroy@gmail.com>
2018-02-07 09:44:52 +0530

    Adding few more problems.

---
+++ src/main/java/io/github/kaushikrroy/programmers/java/notes/random/MinimumDifference.java
@@ -0,0 +1,11 @@
+package io.github.kaushikrroy.programmers.java.notes.random;
+
+import java.util.Arrays;
+
+public class MinimumDifference {
+
+    public int minimumDifference(final int[] input) {
+        
+        Arrays.sort(input);
+    }
+}
